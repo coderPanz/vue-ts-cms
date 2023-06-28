278c5130db0e3171588cc581f6eb36fa23a0ac41
coderPanz <3108498426@qq.com>
2023-06-28 20:13:11 +0800

    cms-6-28

--- src/store/main/admin.ts
+++ src/store/main/admin.ts
@@ -1,5 +1,6 @@
-import { getUserListReq, deleteIdUserReq } from '@/server/index'
+import { getUserListReq, deleteIdUserReq, createUserReq } from '@/server/index'
 import { defineStore } from "pinia";
+import type { IDialogForm } from '@/types/Dialog/dialogForm'
 
 interface IState {
   userList: any[]
@@ -11,14 +12,19 @@ const useAdminStore = defineStore('admin', {
     count: 0
   }),
   actions: {
-    // 获取用户列表的网络请求
+    // 1. 获取用户列表的网络请求
     async fetchGetUserList(data: any) {
       const res = await getUserListReq(data)
       this.userList = res.data.data
       this.count = res.data.totalCount
     },
+    // 2. 删除用户后重新发送网络请求获取最新的数据
     async fetchDeleteUserList(id: any) {
       const res = await deleteIdUserReq(id)
+    },
+    // 3. 创建用户
+    async fetchCreateUser(data: IDialogForm) {
+      const res = await createUserReq(data)
     }
   }
 })
