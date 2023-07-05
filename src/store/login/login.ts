d5d19c3999a21d4d8f2ae3775eebc8265a26f2ce
學吳紫荆 <125804580+coderPanz@users.noreply.github.com>
2023-07-05 18:04:59 +0800

    cms-7-05

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -24,22 +24,24 @@ const useLoginStore = defineStore('login', {
         // 封装工具进行本地缓存
         localIns.setCache('token', res.data.token)
         this.token = res.data.token
+        console.log(res.data.id)
         // 1. 获取用户roles数组中的_id
-        const id = res.data.id
-        const userInfos = await userInfosReq(id)
-        const role_id = userInfos.data.data.roles[0]
+        const role_id = res.data.id
+        console.log(role_id)
+        // const userInfos = await userInfosReq(id)
+        // const role_id = userInfos.data.data.roles[0]
         // // 2. 获取角色id
-        const roleInfos = await roleInfosReq(role_id)
-        const roleId = roleInfos.data.data.id
+        // const roleInfos = await roleInfosReq(role_id)
+        // const roleId = roleInfos.data.data.id
         // // 3. 通过角色id获取菜单树
-        const menuTreeData = await menuTreeReq(roleId)
-        localIns.setCache('localMenuTree', menuTreeData.data)
-        this.localMenuTree = menuTreeData.data
+        // const menuTreeData = await menuTreeReq(roleId)
+        // localIns.setCache('localMenuTree', menuTreeData.data)
+        // this.localMenuTree = menuTreeData.data
 
         // // 登录成功后显示main页面前动态添加路由
-        increaseRoute(menuTreeData.data)
+        // increaseRoute(menuTreeData.data)
         // // 4. 跳转页面
-        router.push('/main')
+        // router.push('/main')
       } catch (error) {
         console.error()
       }
